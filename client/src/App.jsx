acf9589f6dd89ba42b8f60311d701f7d43605fc3
PAVANSAI2468 <pavansarapu85@gmail.com>
2024-09-27 01:14:59 +0530

    resetpassword

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Signup from "./components/signup"
-import Login from "./components/login"
-import ForgotPassword from "./components/forgotPassword"
-import Dashboard from "./components/dashboard"
-import ResetPassword from "./components/resetpassword"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/signup";
+import Login from "./components/login";
+import ForgotPassword from "./components/forgotPassword";
+import Dashboard from "./components/dashboard";
+import ResetPassword from "./components/resetpassword"; // Update the import if file names differ
+
 function App() {
   return (
-       <BrowserRouter basename="/">
-       <Routes>
-        <Route exact path="/signup" element={<Signup/>}></Route>
-        <Route exact path="/login" element={<Login/>}></Route>
-        <Route exact path="/forgot_password" element={<ForgotPassword/>}></Route>
-        <Route exact path="/resetpassword" element={<ResetPassword/>}></Route>
-        <Route exact path="/" element={<Dashboard/>}></Route>
-       </Routes>
-       </BrowserRouter>
-  )
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route
+          exact
+          path="/forgot_password"
+          element={<ForgotPassword />}
+        ></Route>
+        <Route exact path="/resetpassword" element={<ResetPassword />}></Route>{" "}
+        {/* Route should match exactly */}
+        <Route exact path="/" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
